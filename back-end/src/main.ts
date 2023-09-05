24a9884d58f2c57629612ee2105da9857531f1eb
jotaviobueno <joseotaviocbueno@gmail.com>
2023-09-04 21:18:31 -0300

    first commit

---
+++ back-end/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors();
+
+  await app.listen(3000);
+}
+bootstrap();
